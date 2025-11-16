45edef9bb6753a449264b8d45c3512fad243c390
griffinjoshs <leveloutusa@gmail.com>
2025-11-16 16:15:00 -0500

    better form spacing

--- src/content/siteData.ts
+++ src/content/siteData.ts
@@ -1,11 +1,13 @@
 // src/siteData.ts - Compatible with both Astro and React
 const siteDomain = import.meta.env.PUBLIC_SITE_DOMAIN;
+import LogoImage from '../assets/cropped-koi-crest-JPEG-scaled-1.jpg';
 
 export const siteData = {
-  title: "Koi Crest Marketing",
+  title: "Koi Crest",
   legalName: "Koi Crest LLC",
   tagline: "Veteran Owned and Operated Marketing Agency.",
   description: "At Koi Crest, we specialize in comprehensive digital marketing services including Lead Generation, Content Creation and Social Media Management, etc. We will propel your business past the competition. Kickstart your growth with a free consultation and unlock your business’s potential and help you digitally swim past your competition!",
+  Logo: LogoImage,
   domain: siteDomain,
   url: `https://${siteDomain}`,
   location: "New Jersey, USA",
